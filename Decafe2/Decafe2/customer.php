de28f591b0930dc3090fa91743049c1f502959e8
HAYATUL NUFUS <99703346+nufus1102@users.noreply.github.com>
2022-12-01 16:31:07 +0700

    Add files via upload

---
+++ Decafe2/Decafe2/customer.php
@@ -0,0 +1,15 @@
+<div class="col-lg-9 mt-2">
+                <div class="card">
+                    <div class="card-header">
+                        Customer
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Ini adalah bagian Customer</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
+                        standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
+                        a type specimen book.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
